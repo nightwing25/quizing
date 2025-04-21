243e325dff763c3349810a0e9bfc57aceb9deeba
nightwing25 <omarflux30@gmail.com>
2025-04-21 02:21:25 -0400

    adding subject files

--- lessons/trouble.py
+++ lessons/trouble.py
@@ -8,6 +8,7 @@ questions = (
         "what do you most likely do before you test the theory?",#1
         "what should most likely be the first step you do when you encounter a loud clicking noise from the hard drive?",#2
         "whats the last step of trouble shooting once the problem is fixed?",#3
+        "whats in charge of testing major components before booting the operating system?"#4
         )
 
 
@@ -15,7 +16,8 @@ options = (
         ("a.implement solution","b.establish a plan","c.identify the problem","d.establish a theory"),#0
         ("a.verify full system functionality","b.create a plan","c.implement a solution","d.establish a theory"),#1
         ("a.create a backup","b.turn off pc and pull the plug","c.remove the hard drive","d.None of the above"),#2
-        ("a.identify the problem","b.document findings","c.verify full system functionality","d.implement a solution")#3
+        ("a.identify the problem","b.document findings","c.verify full system functionality","d.implement a solution"),#3
+        ("a.Bios","b.POST","c.hardrive","d.cpu")#4
         )
 
 
@@ -23,6 +25,7 @@ answers = (
         "c",#0
         "d",#1   
         "a",#2   
-        "b"#3   
+        "b",#3
+        "b"#4
            )
 
