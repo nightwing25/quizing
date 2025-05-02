14fb2b0ea275a3eec793327933050e758528822b
nightwing25 <omarflux30@gmail.com>
2025-05-02 15:58:57 -0400

    added some more questions

--- lessons/wireless.py
+++ lessons/wireless.py
@@ -2,17 +2,17 @@
 
 
 questions = (
-        "",#
-        "",#
-        "",#
-        "",#
-        "",#
-        "",#
-        "",#
-        "",#
-        "",#
-        "",#
-        "",#
+        "what companies used CDMA?",#
+        "which mobile network brought mobile tv,GPS,video on demand? ",#
+        "why was LTE and 4G created?",#
+        "how did CDMA communicate?",#
+        "how did GSM communicate?",#
+        "LTE-A supported download rates of __?",#
+        "what does EDGE stand for?",#
+        "what did the PRL(perferd roaming list) do?",#
+        "what were 5G improvements?",#
+        "extending your cellular data like a wifi to other devices is an example of what?",#
+        "what is an MDM used for?",#
         "",#
         "",#
         ""#
