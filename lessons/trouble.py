f5c74e4d8f0fd98ce377005e70ce051657ed4ee7
nightwing25 <omarflux30@gmail.com>
2025-05-04 19:03:22 -0400

    adding more questions and answers

--- lessons/trouble.py
+++ lessons/trouble.py
@@ -11,9 +11,13 @@ questions = (
         "this is built-in to most drive arrays usually used to check the status(health info) of the disk what is this called?",#8
         "array will work aslong as one drive is operational what raid does this describe the most?",#9
         "need all drives operational but one, which raid device describes this the best?",#10
-        "what is used to measure our overall performance of our storage drives?, this is a broad metric of our maximum performance"#11
-        )
-
+        "what is used to measure our overall performance of our storage drives?, this is a broad metric of our maximum performance",#11
+        "what does a pxiel shift do?",#12
+        "LCD's have image sticking but what is this for?",#13
+        "what is the best solution to fix dead pixels on the screen?",#14
+        "what could be the main reason for why a printer prints gibberish?",#15
+        "whats the best troubleshooting solution for when inkjet printers print a line on the paper?",#16
+        "whats the best troubleshooting solution for when laser printers print a line on the paper?"#17
 
 options = (
         ("a.implement solution","b.establish a plan","c.identify the problem","d.establish a theory"),#0
@@ -27,7 +31,13 @@ options = (
         ("a.S.M.A.R.T","b.IOPS","c.top","d.htop"),#8
         ("a.RAID 0","b.RAID 10(1+0)","c.RAID 1","d.RAID 5"),#9
         ("a.RAID 0","b.RAID 1","c.RAID 5","d.RAID 10(1+0)"),#10
-        ("a.task manager","b.IOPS","c.S.M.A.R.T","d.top"),#1
+        ("a.task manager","b.IOPS","c.S.M.A.R.T","d.top"),#11
+        ("a.some displays have this to help prevent burn-in","b.removes dead pixels by displaying a white screen","c.its burn-in","d.a manufacturing defect"),#12
+        ("a.LCD's tries to remove burn in by displaying white screen for an extended period of time","b.burn-in","c.pixel shift","d.native resolution"),#13
+        ("a.image sticking","b.pixel shift","c.replace the screen","d.clean with damp cloth"),#14
+        ("a.new update","b.its using PCL","c.its using postscript","d.bad printer driver/model"),#15
+        ("a.clean print heads","b.clean toner","c.replace paper","d.update print drivers"),#16
+        ("a.replace toner cartdridge","b.check for scratches on the photo sensitive drum","c.replace printer","d.update print drivers")#17
         )
 
 
@@ -44,5 +54,11 @@ answers = (
         "c",#9
         "c",#10
         "b",#11
+        "a",#12
+        "a",#13
+        "c",#14
+        "d",#15
+        "a",#16
+        "b"#17
            )
 
