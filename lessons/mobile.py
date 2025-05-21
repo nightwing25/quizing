8963552113bae0b9167e2410a65a6b5dc36161e3
nightwing25 <omarflux30@gmail.com>
2025-05-21 15:55:51 -0400

    adding missing file

---
+++ lessons/mobile.py
@@ -0,0 +1,35 @@
+
+
+
+questions = (
+        "this type of stylus is good for handwriting,note taking and signatures for percise function.",#1
+        "this is an active stylus has more advanced tools and can function independently from device (pressure sensitvity,programmable buttons etc.)",#2
+        "this type of stylus must be compatible with with device propritery.",#3
+        "what is best used for when you dont have a touch screen what styuls is best used for still get percise input?",#4
+        "with this piece of hardware you can replace your mouse can also be external what is it?",#5
+        "this is device used for external keyboard/mouse,extend laptop interfaces,add functionality,desktop adssapter cards avoids cable issues",#6
+        "this is similar to a docking station doesnt commonly have an expansion card option, usually connects via usb.",#7
+        )
+
+
+options = (
+        ("a.touch pens","b.digital stylus","c.active stylus","d.track pad"),#1a
+        ("a.active stylus","b.touch pens","c.drawing pad","d.digital stylus"),#2d
+        ("a.capacitive stylus","b.track pad","c.digital stylus","d.mdm"),#3c
+        ("a.external tablet","b.capacitive stylus","c.drawing pad","d.track pad"),#4c
+        ("a.touch pens","b.drawing pad","c.mouse","d.track pad"),#5d
+        ("a.usb","b.110 block","c.port replicator","d.docking station"),#6d
+        ("a.port replicator","b.docking station","c.usb","d.110 block")#7a
+        )
+
+
+answers = (
+        "a",#1    
+        "d",#2   
+        "c",#3    
+        "c",#4    
+        "d",#5    
+        "d",#6    
+        "a"#7    
+           )
+
